5ed8db602b27c7de74e0bc64f5ab94716c0f8652
Ticha23 <mutwirigachanja1@gmail.com>
2023-02-17 06:15:25 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /*
+ * main
  * printing a word
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
